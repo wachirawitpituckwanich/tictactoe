36d000eb5066caefd662927fa9fdc2a39eb0c130
Wachirawit Pituckwanich <58930041+wachirawitpituckwanich@users.noreply.github.com>
2022-04-06 01:23:34 +0700

    Update helper.js

--- src/components/game/helper.js
+++ src/components/game/helper.js
@@ -1,6 +1,4 @@
 export function allElementSame(array) {
-    // Ultimately got this solution from stack overflow, I had a lot of difficulty getting it to work on my own
-
     var first = array[0];
 
     if (array[0] == null) {
